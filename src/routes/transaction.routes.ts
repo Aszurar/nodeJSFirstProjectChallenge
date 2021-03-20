18c5d27bdcb034939f01291ade67d85d31abafdd
Lucas de Lima <lms.souza39@gmail.com>
2021-03-20 17:41:52 -0300

    :tada: first nodejs project challenge finished! :tada:

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,20 @@
 import { Router } from 'express';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
-
+const transactionsRepository = new TransactionsRepository();
 const transactionRouter = Router();
-
-// const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
     // TODO
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+    const results = {
+      transactions,
+      balance,
+    };
+    return response.json(results);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,7 +22,20 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    // recebendo os dados do front-end
+    const { title, value, type } = request.body;
+
+    // instanciando o serviço de criação das instâncias de transação
+    const createTransaction = new CreateTransactionService(
+      transactionsRepository,
+    );
+
+    // criando uma instâmcia de transação e salvando no vetor de transações
+    // do repositório.
+    const transaction = createTransaction.execute({ title, value, type });
+
+    // retornando a transação recem criada
+    return response.json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,27 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
+  public execute({ title, value, type }: Request): Transaction {
     // TODO
+    const typeValidate = ['income', 'outcome'].includes(type);
+    if (!typeValidate) {
+      throw new Error('O tipo é só número seu merda');
+    }
+
+    const typeOfTransaction = type === 'outcome';
+    const valueValidate =
+      this.transactionsRepository.getBalance().total < value;
+
+    if (typeOfTransaction && valueValidate) {
+      throw new Error('Ta sem grana seu merda');
+    }
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
